ad2e495fad78665539fe754415e6fad6ea663612
Barbara Muesing <muesingb@gmail.com>
2019-09-05 01:01:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def monopoly_with_third_tier
 	{:railroads => {
 		:pieces => 4,
 		:rent_in_dollars => {
-			:pass_go => 100, :collect => 100, :save => 100, :go => 100}
+			:pass_go => 100, :collect => 100, :save => 100, :go => 100},
+		:names => {}
 		}
 	}
 end
