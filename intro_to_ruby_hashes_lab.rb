a70bdc4d7102623e22227aedff8a75daf5b4328d
Barbara Muesing <muesingb@gmail.com>
2019-09-05 01:03:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
 	{:railroads => {
 		:pieces => 4,
 		:rent_in_dollars => {
-			:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 100},
+			:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
 		:names => {:key1 => 1, :key2 => 2, :key3 => 3, :key4 => 4}
 		}
 	}
